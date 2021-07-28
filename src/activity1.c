e56fd0b3028e06717eb51621e64e2b9960d85822
Lalitha74 <86546222+Lalitha74@users.noreply.github.com>
2021-07-28 19:45:49 +0530

    Update activity1.c

--- src/activity1.c
+++ src/activity1.c
@@ -1,13 +1,4 @@
-/**
- * @file activity1.c
- * @author 260894 (you@domain.com)
- * @brief
- * @version 0.1
- * @date 2021-04-23
- *
- * @copyright Copyright (c) 2021
- *
- */
+
 
 #include <avr/io.h>
 #include <util/delay.h>
