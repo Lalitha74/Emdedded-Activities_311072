f31ca2ad1a244d597cc0b1264129663280776893
Lalitha74 <86546222+Lalitha74@users.noreply.github.com>
2021-07-31 20:27:54 +0530

    Update activity2.h

--- inc/activity2.h
+++ inc/activity2.h
@@ -1,14 +1,3 @@
-/**
- * @file activity2.h
- * @author 260894 (you@domain.com)
- * @brief
- * @version 0.1
- * @date 2021-04-27
- *
- * @copyright Copyright (c) 2021
- *
- */
-
 #ifndef ACTIVITY2_H_INCLUDED
 #define ACTIVITY2_H_INCLUDED
 
